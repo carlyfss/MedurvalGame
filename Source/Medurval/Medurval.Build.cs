931dcc3818143eb963eb02812abc4502bbf4eef1
Carly Silva <carly.fs.silva@gmail.com>
2021-11-03 19:46:56 -0300

    Added setup to Gameplay Abilities System

--- Source/Medurval/Medurval.Build.cs
+++ Source/Medurval/Medurval.Build.cs
@@ -7,14 +7,16 @@ public class Medurval : ModuleRules
 	public Medurval(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[]
+			{"GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore"});
+		PublicDependencyModuleNames.AddRange(new string[] {"UMG"});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
