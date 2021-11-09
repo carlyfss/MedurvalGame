12be62ffd4ee5fcdb2854b8fb6f108521fed0a02
Carly Silva <carly.fs.silva@gmail.com>
2021-11-09 17:06:23 -0300

    Added some static meshes Added BaseInventorySlot Widget class

---
+++ Plugins/GameFeatures/InventoryManager/Source/Private/UI/_Base/BaseInventorySlot.h
@@ -0,0 +1,17 @@
+// MEDURVAL PROJECT copyrighted code by Fireheet Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseInventorySlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UBaseInventorySlot : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
